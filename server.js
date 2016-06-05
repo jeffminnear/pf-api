001fb29c35ed08c61c0a0724e1dfc25261df82ef
Jeff Minnear <jeffminnear@gmail.com>
2016-06-05 12:09:57 -0400

    Return search results as JSON

--- server.js
+++ server.js
@@ -6,6 +6,8 @@
 const express     = require('express');
 const app         = express();
 const bodyParser  = require('body-parser');
+const scrapers    = require('pf-scrapers');
+const _           = require('lodash');
 
 // configure app to use bodyParser()
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,12 +21,35 @@ const port = process.env.PORT || 8080;
 
 const router = express.Router();    // get an instance of the express router
 
+// middleware to use for all requests
+router.use( (req, res, next) => {
+  console.log("Request received...");
+  next();
+});
+
 // test route to verify connection (accessed at GET http://localhost:8080/api)
 router.get('/', (req, res) => {
   res.json({ message: "Connection to Pilot-Fish API was successful" });
 });
 
+// routes will end with /search
+router.route('/search')
 
+  // perform a search
+  .post( (req, res) => {
+    let args = {
+      limit: req.body.limit == null ? 1 : req.body.limit,
+      title: req.body.title
+    };
+
+    let promises = scrapers;
+
+    Promise.all(promises.map( (x) => x(args)))
+      .then( (results) => {
+        results = _.flatten(results);
+        res.json(results);
+      });
+  });
 
 // REGISTER ROUTES --------------------------
 // all routes will be prefixed with /api
